7228e13e4ce579cae4031e63f032954ae003d5a6
SlavickKuzmin <slvick.kuzmin2014@gmail.com>
2016-08-01 21:50:11 +0300

    Lesson 2.

---
+++ ShadersTool.h
@@ -0,0 +1,12 @@
+//
+// Created by slavickkuzmin on 8/1/16.
+//
+
+#ifndef GL3_0_SHADERSTOOL_H
+#define GL3_0_SHADERSTOOL_H
+
+#include <GL/gl.h>
+
+GLuint LoadShaders(const char *vertex_file_path, const char *fragment_file_path);
+
+#endif //GL3_0_SHADERSTOOL_H
--- main.cpp
+++ main.cpp
@@ -3,7 +3,9 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <glm/glm.hpp>
+#include "ShadersTool.h"
 using namespace glm;
+GLFWwindow *window;
 
 int main()
 {
@@ -19,14 +21,15 @@ int main()
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    GLFWwindow *window ;
-    window = glfwCreateWindow(1024, 768, "Tutorial 1", NULL, NULL);
+    window = glfwCreateWindow(1024, 768, "Tutorial 2 - red cube", NULL, NULL);
+
     if(window == NULL)
     {
         fprintf(stderr, "GL error, version not supported!");
         glfwTerminate();
         return -1;
     }
+
     glfwMakeContextCurrent(window);
 
     glewExperimental = true;
@@ -35,12 +38,61 @@ int main()
         fprintf(stderr, "GLEW init error!");
         return -1;
     }
+
     glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+
+    glClearColor(0.f, 0.f, 0.4f, 0.f);
+
+    //Create VBO
+    GLuint VertexArrayID;
+    glGenVertexArrays(1, &VertexArrayID);
+    glBindVertexArray(VertexArrayID);
+
+    GLuint programID = LoadShaders("/home/slavickkuzmin/lessons/gl3_0/simpleVertex.glsl",
+                                   "/home/slavickkuzmin/lessons/gl3_0/simpleFragment.glsl");
+    //
+    static const GLfloat g_vertex_buffer_data[] = {
+           -1.f,  -1.f,  0.f,
+            1.f,  -1.f, 0.f,
+            0.f,   1.f,  0.f
+    };
+
+    GLuint vertexbuffer;
+    glGenBuffers(1, &vertexbuffer);
+    glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
+
+
+
+
+
+    //main cycle
     do
     {
+        glClear(GL_COLOR_BUFFER_BIT);
+        glUseProgram(programID);
+        glEnableVertexAttribArray(0);
+        glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+        glVertexAttribPointer(0,        //Шейдери
+                              3,        //Размер
+                              GL_FLOAT, //Тип
+                              GL_FALSE, //Указывает, что значения не нормализованы
+                              0,        //Шаг
+                              (void*)0  //Смещение масива в буффере
+        );
+        //Вывести треугольник
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDisableVertexAttribArray(0);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
     } while(glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS &&
             glfwWindowShouldClose(window) == 0);
+    // Cleanup VBO
+    glDeleteBuffers(1, &vertexbuffer);
+    glDeleteVertexArrays(1, &VertexArrayID);
+    glDeleteProgram(programID);
+
+    // Close OpenGL window and terminate GLFW
+    glfwTerminate();
 }
